a45cdd606cd915a24099fa3d7cbd88dc00254b77
logan <logan.testa@outlook.com>
2019-12-02 12:56:10 -0800

    Adjust general nav and style link paths

--- assets/include/navigation-content.php
+++ assets/include/navigation-content.php
@@ -2,14 +2,14 @@
     <div class="inner-wrapper">
         <ul>
             <li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="about.php">About</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="location.php">Location</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="products.php">Products</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="recipes.php">Recipes</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="news-and-events.php">News/Events</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="careers.php">Careers</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="blog.php">Blog</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="contact-us.php">Contact Us</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/about.php">About</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/location.php">Location</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/products.php">Products</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/recipes.php">Recipes</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/news-and-events.php">News/Events</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/careers.php">Careers</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/blog/blog.php">Blog</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/contact-us.php">Contact Us</a></li>
             </li>
         </ul>
     </div>
@@ -18,14 +18,14 @@
     <div id="dropdownButton"></div>
     <div id="dropdownContent">
         <ul>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="about.php">About</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="location.php">Location</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="products.php">Products</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="recipes.php">Recipes</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="news-and-events.php">News/Events</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="careers.php">Careers</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="blog.php">Blog</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="contact-us.php">Contact Us</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/about.php">About</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/location.php">Location</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/products.php">Products</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/recipes.php">Recipes</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/news-and-events.php">News/Events</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/careers.php">Careers</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/blog/blog.php">Blog</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="/TualatinTopBakery/contact-us.php">Contact Us</a></li>
         </ul>
     </div>
 </nav>
