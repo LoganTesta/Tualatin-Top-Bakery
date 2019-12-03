97fe8da6ebb872c346c71520a370d2182bb2ad9f
logan <logan.testa@outlook.com>
2019-12-03 13:08:22 -0800

    Adjust site links

--- assets/include/navigation-content.php
+++ assets/include/navigation-content.php
@@ -2,14 +2,14 @@
     <div class="inner-wrapper">
         <ul>
             <li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/about.php">About</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/location.php">Location</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/products.php">Products</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/recipes.php">Recipes</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/news-and-events.php">News/Events</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/careers.php">Careers</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/blog/blog.php">Blog</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us.php">Contact Us</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="about.php">About</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="location.php">Location</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="products.php">Products</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="recipes.php">Recipes</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="news-and-events.php">News/Events</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="careers.php">Careers</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="blog/blog.php">Blog</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="contact-us.php">Contact Us</a></li>
             </li>
         </ul>
     </div>
@@ -18,14 +18,14 @@
     <div id="dropdownButton"></div>
     <div id="dropdownContent">
         <ul>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/about.php">About</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/location.php">Location</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/products.php">Products</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/recipes.php">Recipes</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/news-and-events.php">News/Events</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/careers.php">Careers</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/blog/blog.php">Blog</a></li>
-                <li class="nav__nav-item"><a class="nav__nav-link" href="/contact-us.php">Contact Us</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="about.php">About</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="location.php">Location</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="products.php">Products</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="recipes.php">Recipes</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="news-and-events.php">News/Events</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="careers.php">Careers</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="blog/blog.php">Blog</a></li>
+                <li class="nav__nav-item"><a class="nav__nav-link" href="contact-us.php">Contact Us</a></li>
         </ul>
     </div>
 </nav>
