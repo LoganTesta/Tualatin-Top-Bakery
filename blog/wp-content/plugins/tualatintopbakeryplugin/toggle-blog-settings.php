a7dd1f5f2f4607878646919e652ca67be86667b6
logan <logan.testa@outlook.com>
2019-12-03 21:37:09 -0800

    Improve blog plugin

--- blog/wp-content/plugins/tualatintopbakeryplugin/toggle-blog-settings.php
+++ blog/wp-content/plugins/tualatintopbakeryplugin/toggle-blog-settings.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Tualatin Top Bakery: Toggle Blog Settings
- * Plugin URI: https://www.tualatintopbakery.com/toggle-blog-order
+ * Plugin URI: https://www.tualatintopbakery.com/toggle-blog-settings
  * Version: 1.0
  * Author: Tualatin Top Bakery
  * Description: This plugin adds the ability for the user to show or hide blog text to quickly scroll down a long list of blogs without having to skim over the blog content.  The page you are using the plugin on must have an outer blog posts container div with ID of 'blogPostsContainer', and an inner container div with ID of 'blogPosts'.  Each individual blog must have content with a class name of 'blog__content'.  Enjoy!
@@ -14,25 +14,25 @@
 <script text="text/javascript">
     
     window.addEventListener("load", function(){
+        if(document.getElementsByTagName('body')[0].className === "page-blog"){
+            let blogPostsContainer = document.getElementById("blogPostsContainer");
+            let blogPostsDiv = document.getElementById("blogPosts");
+            let orderBlogsDiv = document.createElement('div');   
+            orderBlogsDiv.innerHTML = "Show/hide blogs' body text";
+             orderBlogsDiv.className = "order-blogs";
         
-        let blogPostsContainer = document.getElementById("blogPostsContainer");
-        let blogPostsDiv = document.getElementById("blogPosts");
-        let orderBlogsDiv = document.createElement('div');   
-        orderBlogsDiv.innerHTML = "Show/hide blogs' body text";
-        orderBlogsDiv.className = "order-blogs";
         
+            blogPostsContainer.insertBefore(orderBlogsDiv, blogPostsDiv);
         
-        blogPostsContainer.insertBefore(orderBlogsDiv, blogPostsDiv);
+            document.getElementsByClassName("order-blogs")[0].className +=" show";
         
-        document.getElementsByClassName("order-blogs")[0].className +=" show";
-        
-        document.getElementsByClassName("order-blogs")[0].addEventListener("click", function(){
-           let blogContentList = document.getElementsByClassName("blog__content");
-           for(let i = 0; i<blogContentList.length; i++){
-               blogContentList[i].classList.toggle("hide");
-           }
-        }, "false");
-        
+             document.getElementsByClassName("order-blogs")[0].addEventListener("click", function () {
+                let blogContentList = document.getElementsByClassName("blog__content");
+                for (let i = 0; i < blogContentList.length; i++) {
+                    blogContentList[i].classList.toggle("hide");
+                }
+            }, "false");
+        }
     }, "false");
-    
+
 </script>
