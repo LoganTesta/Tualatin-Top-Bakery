5fa8f659fde1e46f7ec32dd88a0c3dfc8d820258
logan <logan.testa@outlook.com>
2019-12-10 13:49:05 -0800

    Adjust index to redirect to home

--- index.php
+++ index.php
@@ -15,3 +15,7 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+
+//Redirect to the home page.
+header("Location: https://www.tualatintopbakery.com/home.php");
+exit();
