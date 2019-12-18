0ca8cf11e69e0a08a87e782760b0a89dce58b2b7
logan <logan.testa@outlook.com>
2019-12-18 15:25:37 -0800

    Reinstall WordPress

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'blog' );
+define( 'DB_NAME', 'tualatintopbakeryblog' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'l,4cb1ukD<8x::6CW@;[>5GT&TW^P1m(_W|y!h0L?w`B@cogM6A+CXQ%%RM[GH,(' );
-define( 'SECURE_AUTH_KEY',  'Jr~PzlLQpvsg^bTEe_g9o]gNq -CI]ox`]uw=-#-p*oIn]L5]Gin4/>]|VIv0G}h' );
-define( 'LOGGED_IN_KEY',    'WGfJb*Fv/<N+>FK^o3>]x@?UO%L@(vV=!F4(.z/8[|?OWpWT8y7U{6wZ:jS=L^2K' );
-define( 'NONCE_KEY',        'exT41d6(;fZv S,9aM^&@:_.=|E.(m9j&?-C6C-_G=~=N = Q=^Yk#~Y6Ym]7]d>' );
-define( 'AUTH_SALT',        'aCF(gsFlnmhxrqAMhtUGs3nv?^N:a]y?`^U8NK3]f~S,cRK%Qwtg7p~.btN!%1u%' );
-define( 'SECURE_AUTH_SALT', ';,JX5_6, `4L7Ju$5ECt+:C(1gn ](O+!yc&5Wb|dx=+o`T,WoB~6.15S.nWY#2G' );
-define( 'LOGGED_IN_SALT',   't=d,Wfw<$<i(I srlU0&+]n&O>T+U3lPzXhNb@Tile1ssQeK$QIlakxwyz41v&5x' );
-define( 'NONCE_SALT',       'v/cH)fEw?L2n0V*#}?dia7WrCh4FFq|*5Yks#~|dJ#9s+J)3LVelEH!G:qH>( J!' );
+define( 'AUTH_KEY',         'uV7hRQptT1>85$%z,rgztOC%89c s4a|b1/}>W5fCK1cDhan8k[2,j}D:SEZkNpv' );
+define( 'SECURE_AUTH_KEY',  'O``T[~gaTc>XY<AvA`xY32cKn_}R>/n3Kw>+SMZr]p+Vq1tCe}s`#mX:n+%/+,:D' );
+define( 'LOGGED_IN_KEY',    'sqJ3U$IlUFlMK/2^%#<u#?z:YX0N[_4@ 8;O9;6(29x_Ps`.);jKZ([+uiTZ@`Tx' );
+define( 'NONCE_KEY',        'EqNROM[j4r(0$&yW<BU~lP)D2/z<fOYvkw;L.y/0VTAO_NkvA{,dI0ob=R>(qyB4' );
+define( 'AUTH_SALT',        'R$^,#wJ2l,_^}O3O0YX5FOO-,+[:k4~6-^nGu*(X* yOuT)xnj[h5TDf-#vQ}!0;' );
+define( 'SECURE_AUTH_SALT', '7+1qMgh7KiFZnq[o%pU]MDHVmQff;6RsZth)Fas~b7,BY<WGc9WT7K08r@9DO]5X' );
+define( 'LOGGED_IN_SALT',   'x}AZ0*,c5BL>6j16$2_nWV.Z`E1F(kdfm~0Nb@m7!Cva<i5php8~35l$`nR+t,z=' );
+define( 'NONCE_SALT',       '-XA *DgN+n5})Vsp8Hzi8Vq&@?iO5vd8I{!|-[Y</zj6))ep+NK=1AI{rU[LSOWC' );
 
 /**#@-*/
 
