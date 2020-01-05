4f65bb53dcc3eed4e0288b36ca18e06b2a0a5ef5
logan <logan.testa@outlook.com>
2020-01-05 12:57:05 -0800

    Adjust theme functions formatting

--- wordpress/wp-content/themes/tualatintopbakery/functions.php
+++ wordpress/wp-content/themes/tualatintopbakery/functions.php
@@ -1,10 +1,10 @@
 <?php
 
-add_theme_support('post-thumbnails'); //Allow image thumbnails in pages and posts.
-
+add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
 //Allow cropping for medium thumbnail images.
-if(false === get_option("medium_crop")) {
-    add_option("medium_crop", "1");
+
+if(false === get_option( "medium_crop" )) {
+    add_option( "medium_crop", "1" );
 } else {
-    update_option("medium_crop", "1");
+    update_option( "medium_crop", "1" );
 }
