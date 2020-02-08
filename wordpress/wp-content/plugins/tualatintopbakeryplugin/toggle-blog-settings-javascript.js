a01aea1dcaa0e436d8ccc92107fc6274a96f8aa6
logan <logan.testa@outlook.com>
2020-02-08 11:35:42 -0800

    Adjust .htaccess and blog settings plugin

--- wordpress/wp-content/plugins/tualatintopbakeryplugin/toggle-blog-settings-javascript.js
+++ wordpress/wp-content/plugins/tualatintopbakeryplugin/toggle-blog-settings-javascript.js
@@ -1,14 +1,14 @@
 
 window.addEventListener("load", function () {
 
-        let blogControls = document.getElementById("blogControls");
-        let numberOfControls = document.getElementsByClassName("blog-controls__control").length-1;
-        let blogPostsDiv = document.getElementsByClassName("blog-controls__control")[numberOfControls];
-        let orderBlogsDiv = document.createElement('div');
-        orderBlogsDiv.innerHTML = "Show/hide blogs' body text";
-        orderBlogsDiv.className = "order-blogs";
+    let blogControls = document.getElementById("blogControls");
+    let numberOfControls = document.getElementsByClassName("blog-controls__control").length - 1;
+    let blogPostsDiv = document.getElementsByClassName("blog-controls__control")[numberOfControls];
+    let orderBlogsDiv = document.createElement('div');
+    orderBlogsDiv.innerHTML = "Show/hide blogs' body text";
+    orderBlogsDiv.className = "order-blogs";
 
-
+    if (blogControls !== null) {
         blogControls.insertBefore(orderBlogsDiv, blogPostsDiv.nextSibling);
 
         document.getElementsByClassName("order-blogs")[0].className += " show";
@@ -18,11 +18,11 @@ window.addEventListener("load", function () {
             for (let i = 0; i < blogContentList.length; i++) {
                 blogContentList[i].classList.toggle("hide");
             }
-            
+
             let blogImageList = document.getElementsByClassName("blog__image");
             for (let i = 0; i < blogImageList.length; i++) {
                 blogImageList[i].classList.toggle("smaller");
             }
         }, "false");
-        
+    }
 }, "false");
