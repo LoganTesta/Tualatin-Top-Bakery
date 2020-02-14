29822a90a3a83ea958a674b8849a893ad6987c20
logan <logan.testa@outlook.com>
2020-02-13 21:00:43 -0800

    Improve testimonials settings input handling

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'tualatintopbakeryblog' );
+define( 'DB_NAME', 'tualatintopbakery' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
