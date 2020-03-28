faf1d02bb50aa65923f93774baf263f3802cb0d2
logan <logan.testa@outlook.com>
2020-03-28 12:36:40 -0700

    Rework estimate cart structure

--- products.php
+++ products.php
@@ -361,69 +361,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr class="estimate-table__item-zero <?php if($_SESSION["quantity"][0] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $WholeWheatLoaf->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $WholeWheatLoaf->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][0]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][0]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>  
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                       
-                                        </tr>
-                                        <tr class="estimate-table__item-one <?php if($_SESSION["quantity"][1] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $WhiteBreadLoaf->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $WhiteBreadLoaf->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][1]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][1]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>  
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                        
-                                        </tr>
-                                        <tr class="estimate-table__item-two <?php if($_SESSION["quantity"][2] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $BlueberryScone->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $BlueberryScone->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][2]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][2]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td> 
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                              
-                                        </tr>
-                                        <tr class="estimate-table__item-three <?php if($_SESSION["quantity"][3] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $ChocolateCake->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $ChocolateCake->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][3]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][3]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>  
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                             
-                                        </tr>
-                                        <tr class="estimate-table__item-four <?php if($_SESSION["quantity"][4] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $CherryPie->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $CherryPie->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][4]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][4]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>  
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                          
-                                        </tr>
-                                        <tr class="estimate-table__item-five <?php if($_SESSION["quantity"][5] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $BlueberryPie->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $BlueberryPie->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][5]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][5]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>   
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                         
-                                        </tr>
-                                        <tr class="estimate-table__item-six <?php if($_SESSION["quantity"][6] <= 0){ echo "hide"; } ?>">
-                                            <td class="estimate-table__item-title"><?php echo $BlueberryMuffin->get_name(); ?></td>
-                                            <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
-                                            <td class="estimate-table__item-cost">$<?php echo $BlueberryMuffin->get_price(); ?></td>
-                                            <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][6]; ?></td>
-                                            <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][6]; ?></td>
-                                            <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>    
-                                            <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                           
-                                        </tr>
+                                        <?php for($i = 0; $i < count($products); $i++) { ?>
+                                            <tr class="estimate-table__item <?php echo $products[$i]->get_classCSS(); ?> <?php if($_SESSION["quantity"][$i] <= 0){ echo "hide"; } ?>">
+                                                <td class="estimate-table__item-title"><?php echo $products[$i]->get_name(); ?></td>
+                                                <td class="estimate-table__item-image"><div class="estimate-table__item-image__photo"></div></td>
+                                                <td class="estimate-table__item-cost">$<?php echo $products[$i]->get_price(); ?></td>
+                                                <td class="estimate-table__item-quantity"><?php echo $_SESSION["quantity"][$i]; ?></td>
+                                                <td class="estimate-table__item-subtotal">$<?php echo $_SESSION["itemSubtotal"][$i]; ?></td>
+                                                <td class="estimate-table__minus"><div class="estimate-table__minus__item">-</div></td>  
+                                                <td class="estimate-table__add"><div class="estimate-table__add__item">+</div></td>                       
+                                            </tr>
+                                        <?php } ?>
                                         <tr class="clear-both"></div>
                                     </tbody>
                                 </table>
