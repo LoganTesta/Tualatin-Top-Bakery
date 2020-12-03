8685c5b9283f02d37bef41094392a6806f162f9d
logan <logan.testa@outlook.com>
2020-12-02 19:34:36 -0800

    Refactor site theme

--- wordpress/wp-content/themes/tualatintopbakery/functions.php
+++ wordpress/wp-content/themes/tualatintopbakery/functions.php
@@ -9,13 +9,13 @@ add_filter( 'rest_authentication_errors', function( $result ) {
        return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => 401 ) );    
    }
    return $result;
-});
+} );
 
 
 add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
 //Allow cropping for medium thumbnail images.
 
-if (false === get_option( "medium_crop" )) {
+if ( false === get_option( "medium_crop" ) ) {
     add_option( "medium_crop", "1" );
 } else {
     update_option( "medium_crop", "1" );
@@ -27,4 +27,4 @@ add_image_size( "medium_rect_crop", 460, 320, true );
 add_action( 'wp_enqueue_scripts', function(){ 
     wp_register_script( 'footer-modal-javascript', get_template_directory_uri() . '/footer-modal-javascript.js' );
     wp_enqueue_script( 'footer-modal-javascript', get_template_directory_uri() . '/footer-modal-javascript.js' );  
-});
+} );
