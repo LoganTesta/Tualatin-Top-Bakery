fc1c961f8e06deb7941c09c13dc51ac12506b0b6
logan <logan.testa@outlook.com>
2023-05-23 21:32:09 -0700

    Add theme support for widgets, post formats

--- wordpress/wp-content/themes/tualatintopbakery/functions.php
+++ wordpress/wp-content/themes/tualatintopbakery/functions.php
@@ -1,8 +1,19 @@
 <?php
 
-add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
-//Allow cropping for medium thumbnail images.
 
+//Theme setup and support.
+if ( function_exists( 'tualatin_top_bakery_setup' ) === false ) {
+    function tualatin_top_bakery_setup() {
+        add_theme_support( "post-thumbnails" ); //Allow image thumbnails in pages and posts.
+        add_theme_support( "post-formats", array( 'aside', 'gallery', 'quote', 'image', 'video' ) ); //Let user customize the post format.
+        add_theme_support( "widgets" );
+        add_theme_support( "widgets-block-editor" );
+    }
+}
+add_action( 'after_setup_theme', 'tualatin_top_bakery_setup' );
+
+
+//Allow cropping for medium thumbnail images.
 if ( false === get_option( "medium_crop" ) ) {
     add_option( "medium_crop", "1" );
 } else {
